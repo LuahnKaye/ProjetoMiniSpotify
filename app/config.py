e4a8fa287dd2ed519cace68515df3651bde0b714
Luahn <115737683+LuahnKaye@users.noreply.github.com>
2024-12-01 17:54:37 -0300

    a

---
+++ app/config.py
@@ -0,0 +1,5 @@
+import os
+
+COSMOS_DB_URI = os.getenv("COSMOS_DB_URI", "<sua-uri>")
+COSMOS_DB_KEY = os.getenv("COSMOS_DB_KEY", "<sua-chave>")
+COSMOS_DB_NAME = os.getenv("COSMOS_DB_NAME", "SpotifyDB")
